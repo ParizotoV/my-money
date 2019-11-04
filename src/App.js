28d3de686c6a6b872536eb8e1602d88f7b001815
Vinicius Parizoto <vini.parizoto@live.com>
2019-11-04 17:11:48 -0300

    Função MAP

--- src/App.js
+++ src/App.js
@@ -26,28 +26,80 @@ const { useGet, usePost, useDelete } = Rest(baseURL);
 
 function App() {
 
-  const data = useGet('movement/2019-08')
-  const [postData, post] = usePost('movement/2019-08')
-  const [deleteData, remove] = useDelete();
+  const data = useGet('month')
+  // const [postData, post] = usePost('movement/2019-08')
+  // const [deleteData, remove] = useDelete();
 
   const saveNew = () => {
-    post({ valor: 10, descricao: 'ola' })
+    // post({ valor: 10, descricao: 'ola' })
   }
 
   const doRemove = () => {
-    remove('/movement/2019-08/-LsFdR5hJwjRQEX1T7O0')
+    // remove('/movement/2019-08/-LsFdR5hJwjRQEX1T7O0')
   }
 
   return (
     <div className="App">
-      <h1>My Money</h1>
-      {JSON.stringify(data)}
-      {data.loading && (<p>Loading...</p>)}
-      <button onClick={saveNew}>Salvar</button>
-      <pre>{JSON.stringify(postData)}</pre>
-      <button onClick={doRemove}>Delete</button>
-      <pre>{JSON.stringify(deleteData)}</pre>
+      <nav className='navbar navbar-light bg-light'>
+        <div className='container'>
+          <a className='navbar-brand'>My Money</a>
+        </div>
+      </nav>
+      <div className='container'>
+        <h2>Adicionar mês</h2>
 
+        <select>
+          <option value='2019'>2019</option>
+          <option value='2020'>2020</option>
+        </select>
+        <select>
+          <option value='01'>01</option>
+          <option value='02'>02</option>
+        </select>
+
+        <button>Adicionar mês</button>
+        {
+          data.loading && <span>Carregando...</span>
+        }
+        {
+          !data.loading && (
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>Mês</th>
+                  <th>Previsão entrada</th>
+                  <th>Entrada</th>
+                  <th>Previsão saída</th>
+                  <th>Saída</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  Object
+                    .keys(data.data)
+                    .map(mes => {
+                      return (
+                        <tr id={mes}>
+                          <td>{mes}</td>
+                          <td>{data.data[mes].previsao_entrada}</td>
+                          <td>{data.data[mes].entrada}</td>
+                          <td>{data.data[mes].previsao_saida}</td>
+                          <td>{data.data[mes].saida}</td>
+                        </tr>
+                      )
+                    })
+                }
+                <tr>
+                  <td>2019-09</td>
+                  <td>0</td>
+                </tr>
+              </tbody>
+            </table>
+          )
+        }
+
+        <pre>{JSON.stringify(data)}</pre>
+      </div>
     </div>
   );
 }
